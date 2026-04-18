b8ea5e03542b1f3f6387cbc7bb2a711cf936d23f
Vidhi Omar <VIDHI.23FE10CAI00135@muj.manipal.edu>
2026-04-18 14:11:18 +0530

    retrieval improved using normalize_L2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src.embeddings import load_model, get_embeddings
-from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 from src.vector_store import search , create_faiss_index
 from src.llm import load_llm, generate_answer
+import faiss
 
 sentences=["AI is  transforming the world ","Natural Language Processing is a subfield of Machine Learning" , "I love trying new foods"]
 
@@ -15,7 +15,10 @@ index=create_faiss_index(embeddings)
 query = ["What is AI?"]
 query_embedding = model.encode(query)
 
-distances, indices =search(index, np.array(query_embedding), k=1)
+query_embedding = np.array(model.encode(query).astype("float32"))
+faiss.normalize_L2(query_embedding)
+
+distances, indices = search(index, query_embedding, k=1)
 
 context =""
 for i in indices[0]:
--- src/vector_store.py
+++ src/vector_store.py
@@ -1,15 +1,15 @@
 import faiss
 import numpy as np
 
-
 def create_faiss_index(embeddings):
-    dimension = len(embeddings[0]) 
-    index = faiss.IndexFlatL2(dimension)
-    
-    index.add(np.array(embeddings))
-    
-    return index
+    embeddings = np.array(embeddings).astype("float32")
 
+    faiss.normalize_L2(embeddings) 
+
+    index = faiss.IndexFlatIP(embeddings.shape[1])
+    index.add(embeddings)
+
+    return index
 
 def search(index, query_embedding, k=2):
     distances, indices = index.search(query_embedding, k)
